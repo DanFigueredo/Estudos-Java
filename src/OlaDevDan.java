b3200e9ec3c57445265103d846ec49f3eda82f7d
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-12 14:59:31 -0300

    feat: inicializa projeto Java com Hello World
    - Cria classe OlaDevDan com método main
    - Implementa primeiro programa exibindo mensagem no console
    - Configura ambiente inicial no IntelliJ com integração ao GitHub

---
+++ src/OlaDevDan.java
@@ -0,0 +1,5 @@
+public class OlaDevDan {
+    public static void  main (String[] args){
+        System.out.println("Olá, Mundo!");
+    }
+}
--- teste.java
+++ teste.java
@@ -1,4 +1,4 @@
-public class OlaDanielDev {
+public class teste {
     public static void main (String[] args){
         System.out.println("Olá, Mundo");
     }
