4447ece9561acdbf05fbedc4154dc779f4ad0aff
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-13 19:40:37 -0300

    Update aula02TiposPrimitivos.java

--- src/introducaoJava/aula02TiposPrimitivos.java
+++ src/introducaoJava/aula02TiposPrimitivos.java
@@ -2,9 +2,10 @@ package introducaoJava;
 
 public class aula02TiposPrimitivos {
     public static void main(String[] args) {
-        //int, double, float, char, byte, short, long, boolean
+        //Tipos de variaveis primitivas: int, double, float, char, byte, short, long, boolean
+
         int age = 19;
-        if (age > 18){
+        if (age >= 18){
             System.out.println("Voce tem "+age+" anos e é maior de idade");
         }else {
             System.out.println("Voce tem "+age+" anos e é menor de idade");
