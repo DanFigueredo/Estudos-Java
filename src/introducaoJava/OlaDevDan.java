5a9c2862cba91a0f5e745f21782b5f822cff33f2
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-13 19:39:27 -0300

    feat: adiciona exemplo de uso de tipos primitivos e estrutura condicional (if/else)

---
+++ src/introducaoJava/OlaDevDan.java
@@ -0,0 +1,16 @@
+package introducaoJava;
+
+/**
+ * Comentario java docker ( pode ser usado html)
+ */
+public class OlaDevDan {
+
+    public static void  main (String[] args){
+        // comenatrio linha
+        /*
+        comentario de multiplas linhas
+         */
+        System.out.println("Hello, World!");
+
+    }
+}
---
+++ src/introducaoJava/aula02TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package introducaoJava;
+
+public class aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 19;
+        if (age > 18){
+            System.out.println("Voce tem "+age+" anos e é maior de idade");
+        }else {
+            System.out.println("Voce tem "+age+" anos e é menor de idade");
+        }
+
+    }
+}
