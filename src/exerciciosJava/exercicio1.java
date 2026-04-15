733d227bfd73623f93d5ab8cc7772cb78397e5d5
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-15 13:44:25 -0300

    feat: implementa cadastro simples com verificação de maioridade
    - adiciona variáveis para nome, idade e salário
    - implementa lógica condicional para verificar maioridade
    - exibe dados formatados no console

---
+++ src/exerciciosJava/exercicio1.java
@@ -0,0 +1,25 @@
+package exerciciosJava;
+/*
+Daniel Figueredo
+15/04/2026
+Programa que Pegue dados e mostre na saida
+ */
+public class exercicio1 {
+    public static void main(String[] args) {
+        String name = "Daniel";
+        int age = 19;
+        double payChek = 1512.00;
+        String states;
+        if (age >= 18)
+        {
+            states = "Maior de idade";
+        }else{
+            states = "Menor de idade";
+        }
+
+        System.out.println("Nome: "+name +
+                            "\nidade: "+age +
+                            "\nSalario: "+payChek +
+                            "\nStatus: "+ states);
+    }//fim do programa
+}
