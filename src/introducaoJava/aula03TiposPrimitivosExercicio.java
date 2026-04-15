409a2a835b2b82ac27f47194341f9e2fb99f88c4
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-15 13:28:12 -0300

    feat: adiciona exercício com uso de tipos primitivos e String para geração de relatório

---
+++ src/introducaoJava/aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,12 @@
+package introducaoJava;
+
+public class aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Daniel";
+        float payment = 2500.0f;
+        String address = "Rua tal Numero 80";
+        String paymentDate = "14/04/2026";
+        String report = ("Eu " + name + ", morando no endereço: " + address + " Confirmo que recebi o salario: " + payment + " na data: " + paymentDate);
+        System.out.println(report);
+    }
+}
