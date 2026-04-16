1c5157d68dc66a699361c22130ef70612aa469ba
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-16 18:59:30 -0300

    feat: implementa operações matemáticas e validação de paridade
    - realiza divisão com conversão explícita de tipos (casting)
    - utiliza operador módulo para identificar números pares e ímpares
    - aplica operadores relacionais em estrutura condicional

---
+++ src/introducaoJava/aula04Operadores.java
@@ -0,0 +1,32 @@
+package introducaoJava;
+/*
+Daniel Figueredo
+16/04/2026
+Programa para aprender sobre operadores em Java
+ */
+public class aula04Operadores {
+    public static void main(String[] args){
+        int n1 = 10;
+        int n2 = 20;
+        float result = (float) n1 / (float) n2;
+        System.out.println(result);
+
+        //% == resto da divisão
+        //exemplo pra verificar se é par ou impar
+        int num = 10;
+        if (num % 2 == 0){
+            System.out.println(num+ " É par!");
+        }else{
+            System.out.println(num + "É impar!");
+        }
+        /* >< (maior e menor),
+         == (igual),
+         >=<=(maior igual ou menor igual),
+         != (Diferente de ),
+         && (E)
+         || (Ou)
+         ! (Not)
+        */
+
+    }//fim do programa
+}
