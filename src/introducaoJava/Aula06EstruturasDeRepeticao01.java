d12c878d905f4ab2c4a6688044a16a221b59f710
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-23 14:46:30 -0300

    feat: implementa impressão de números pares até 1 milhão com loop
    feat: implementa impressão de números pares até 1 milhão com loop
    
    - utiliza estrutura for para iterar até 1.000.000
    - otimiza lógica incrementando de 2 em 2 para obter apenas números pares
    - remove necessidade de verificação condicional com operador módulo

--- src/introducaoJava/Aula06EstruturasDeRepeticao01.java
+++ src/introducaoJava/Aula06EstruturasDeRepeticao01.java
@@ -1,9 +1,18 @@
 package introducaoJava;
+
 /*
 Daniel Figueredo
+23/02/2026
+Programa que imprimi todos os numeros pares até 1000000
  */
 public class Aula06EstruturasDeRepeticao01 {
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args)
+    {
+        int i;
+        for (i = i; i <= 1000000;i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }//fim do programa
 }
