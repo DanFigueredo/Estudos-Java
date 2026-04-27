29b9d0f45a3c25dbecfdd60c09962a24dd2a3df7
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-27 18:17:26 -0300

    feat: implementa iteração e exibição de elementos de um array
    - inicializa array de inteiros com valores definidos
    - utiliza estrutura de repetição para percorrer os elementos
    - imprime os valores no console

---
+++ src/introducaoJava/aula07Arrays.java
@@ -0,0 +1,22 @@
+package introducaoJava;
+/*
+Daniel Figueredo
+27/04/2026
+Programa que utiliza arrays para imprimir numeros
+ */
+public class aula07Arrays {
+    public static void main(String[] args) {
+        int i;
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
+        for (i = 0; i < numbers.length; i++)
+        {
+            System.out.println(numbers[i]);
+        }
+    }
+}
