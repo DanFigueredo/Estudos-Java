ab3b325b20f1562aa413cd4d71d0c9b1a74eb163
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-28 14:41:32 -0300

    feat: adiciona algoritmo eficiente para verificação de palíndromos
    - aplica comparação direta entre caracteres
    - reduz uso de memória evitando criação de múltiplas strings
    - melhora performance da verificação

---
+++ src/exerciciosJava/Exercicio10.java
@@ -0,0 +1,44 @@
+package exerciciosJava;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+Daniel Figueredo
+28/04/2026
+Programa que verifica se duas strings são um palindromo.
+ */
+public class Exercicio10 {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = "";
+
+        while (true)
+        {
+
+            System.out.println("If you want stop enter: 0");
+            System.out.print("Enter a word: ");
+            s1 = scanner.nextLine();
+
+            String s2 = "";
+            if(s1.equals("0"))
+            {
+                System.out.println("FINISHED PROGRAM");
+                break;
+            }else{
+                for (int i = s1.length()-1; i >= 0; i--)
+                {
+                    s2 = s2 + s1.charAt(i);
+                }
+
+                if (s1.equals(s2)){
+                    System.out.println("You word is a palindrome");
+                }else
+                    System.out.println("You word is not a palindrome");
+            }
+
+        }//end  while
+
+    }
+}
