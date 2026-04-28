aca081fc5abaacfdb47148f714c8b732d0c73e7c
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-28 15:02:28 -0300

    feat: adiciona exemplos básicos de arrays em Java
    - Declara array com tamanho fixo e atribui valores manualmente
    - Declara array com valores já inicializados
    - Percorre arrays com for tradicional e for-each

--- src/introducaoJava/aula07Arrays.java
+++ src/introducaoJava/aula07Arrays.java
@@ -8,6 +8,7 @@ public class aula07Arrays {
     public static void main(String[] args) {
         int i;
         int[] numbers = new int[5];
+        int [] numbers2 = {1,2,3,4,5,6,7};
         numbers[0] = 10;
         numbers[1] = 20;
         numbers[2] = 30;
@@ -17,6 +18,10 @@ public class aula07Arrays {
         for (i = 0; i < numbers.length; i++)
         {
             System.out.println(numbers[i]);
+        }
+
+        for(int num : numbers2){
+            System.out.println(num);
         }
     }
 }
