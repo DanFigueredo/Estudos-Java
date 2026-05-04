1cfe50f8489c9bb53554a57b90af650c53430687
Daniel Figueredo <danielfsa2007@gmail.com>
2026-05-04 14:06:24 -0300

    feat: create basic Calculator class with add method
    - created Calculator class in domain package
    - implemented initial add method without parameters
    - instantiated Calculator object in test class
    - executed method to display result in console

---
+++ src/javacore/introductionmethods/test/CalculatorTest01.java
@@ -0,0 +1,10 @@
+package javacore.introductionmethods.test;
+
+import javacore.introductionmethods.domain.Calculator;
+
+public class CalculatorTest01 {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.add();
+    }
+}
