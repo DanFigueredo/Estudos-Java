ca70e50eda0ec3674e687765c3710b18bab45fa3
Daniel Figueredo <danielfsa2007@gmail.com>
2026-05-14 22:19:01 -0300

    feat: implement book class with parameterized constructor
    - added Book class with private attributes
    - implemented parameterized constructor for book initialization
    - added getter and setter methods for encapsulation
    - implemented method to display book information
    - practiced constructor usage and object-oriented programming concepts

---
+++ src/JavaExercises/Exercise16.java
@@ -0,0 +1,10 @@
+package JavaExercises;
+
+import javacore.builders.domain.Book;
+
+public class Exercise16 {
+    public static void main(String[] args) {
+        Book book = new Book("Little prince", "Monge das tranzas", 240);
+        book.printer();
+    }
+}
