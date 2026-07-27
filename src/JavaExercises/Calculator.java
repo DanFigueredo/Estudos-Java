d86bf1a332ea45eb9a734cfd060b817ce98ee45d
Daniel Figueredo <danielfsa2007@gmail.com>
2026-07-27 11:17:38 -0300

    feat: implement calculator using functional interface and lambda expression

---
+++ src/JavaExercises/Calculator.java
@@ -0,0 +1,7 @@
+package JavaExercises;
+
+public interface Calculator {
+    int sum(int a, int b);
+
+
+}
---
+++ src/JavaExercises/Exercise34.java
@@ -0,0 +1,23 @@
+package JavaExercises;
+
+import java.util.Scanner;
+
+public class Exercise34 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        int y;
+        System.out.print("Write an number: ");
+        x = scanner.nextInt();
+
+        System.out.print("Write another number: ");
+        y = scanner.nextInt();
+
+
+        Calculator sum = (a, b) -> {
+            return a + b;
+        };
+
+        System.out.println(sum.sum(x,y));
+    }
+}
