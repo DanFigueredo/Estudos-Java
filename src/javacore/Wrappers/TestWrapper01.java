ca406fe00a84ad2c3e955413df28d2a05410e007
Daniel Figueredo <danielfsa2007@gmail.com>
2026-09-09 19:20:59 -0300

    feat: add Integer unboxing example

---
+++ src/javacore/Wrappers/TestWrapper01.java
@@ -0,0 +1,9 @@
+package javacore.Wrappers;
+
+public class TestWrapper01 {
+    public static void main(String[] args) {
+        Integer intW = 1;
+        int i = intW;
+        System.out.println(i);
+    }
+}
